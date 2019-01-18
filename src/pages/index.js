39bb5c3e7fd325a760916bc927e79ad6b45bf0bf
Luan Orlandi <orlandi.luan@gmail.com>
2019-01-18 18:20:47 -0200

    Lecture 3 - Gatsby structure compared to React

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/Layout';
 const Home = () => (
   <Layout>
     <h1>Hello world!</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to my new Gatsby site.</p>
     <p>Now go build something great.</p>
   </Layout>
 );
