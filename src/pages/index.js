a92cceb2e2e502e4e6364d62581de17342b43c03
Luan Orlandi <orlandi.luan@gmail.com>
2019-01-18 20:24:15 -0200

    Lecture 5 - Add a summary in your Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,24 @@ import Layout from '../components/Layout';
 
 const Home = () => (
   <Layout>
-    <h1>Hello world!</h1>
-    <p>Welcome to my new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>
+      Luan Orlandi
+    </h1>
+    <h2>
+      Web Developer
+    </h2>
+    <section>
+      <h4>
+        Hello, I am Luan Orlandi!
+      </h4>
+      <p>
+        {`I'm a Web Developer, I work with Front-end creating apps and web pages.
+        I participate in events about technology and I like to study productivity and communication, it helps me stay organized and collaborative.`}
+      </p>
+      <p>
+        I develop projects mostly with JavaScript using React and Node.
+      </p>
+    </section>
   </Layout>
 );
 
