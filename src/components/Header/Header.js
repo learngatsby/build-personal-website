981bac8405e4867d7edcd365e3debd4174479c6c
Luan Orlandi <orlandi.luan@gmail.com>
2019-01-20 14:41:19 -0200

    Lecture 23 - Build a navbar with component state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1
-        style={{
-          margin: 0,
-          color: 'white',
-          textDecoration: 'none',
-        }}
-      >
-        {siteTitle}
-      </h1>
-    </div>
-  </div>
-);
+class Header extends Component {
+  state = {
+    isMenuOpen: false,
+  }
+
+  handleMenu = () => {
+    this.setState(previousState => ({
+      isMenuOpen: !previousState.isMenuOpen,
+    }));
+  }
+
+  closeMenu = () => {
+    this.setState({ isMenuOpen: false });
+  };
+
+  render = () => {
+    const { siteTitle } = this.props;
+    const { isMenuOpen } = this.state;
+    const burguerClass = isMenuOpen ? 'is-active' : '';
+
+    return (
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link className="navbar-item is-size-4" to="/">
+              {siteTitle}
+            </Link>
+            <button
+              className={`navbar-burger ${burguerClass}`}
+              type="button"
+              aria-label="menu"
+              onClick={this.handleMenu}
+            >
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+            </button>
+          </div>
+          <div className={`navbar-menu ${burguerClass}`}>
+            <div className="navbar-end">
+              <Link
+                className="navbar-item is-size-5"
+                to="/"
+                onClick={this.closeMenu}
+              >
+                Home
+              </Link>
+              <Link
+                className="navbar-item is-size-5"
+                to="/projects"
+                onClick={this.closeMenu}
+              >
+                Projects
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
