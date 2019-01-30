046c0c53a70fcfe9fdfea4821bd3d44c14d63f6a
Luan Orlandi <orlandi.luan@gmail.com>
2019-01-29 23:25:58 -0200

    Lecture 29 - 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Layout from 'components/Layout';
+import SEO from 'components/SEO';
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not Found" />
+    <section className="section">
+      <h1 className="title has-text-centered">404: Not Found</h1>
+      <hr />
+      <p className="has-text-centered">
+        You just hit a route that does not exist.
+      </p>
+    </section>
+  </Layout>
+);
+
+export default NotFoundPage;
